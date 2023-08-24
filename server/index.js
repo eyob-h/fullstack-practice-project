ed1f39ef816c2b07a72bd9955f0ae1633908cd02
Eyob <110186516+eyob-h@users.noreply.github.com>
2023-08-24 18:39:19 +0300

    Add user creation functionality

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
+
 
 //Configurations gotten from the packages' github repos.
 const __filename = fileURLToPath(import.meta.url);
@@ -34,3 +36,20 @@ const storage = multer.diskStorage({
   });
   const upload = multer({ storage });
 
+// Routes that includes files
+app.post("/auth/register", upload.single("picture"), register);
+
+
+
+//Mongoose module setup
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));// call back function to display the port 
+  })
+  .catch((error) => console.log(`${error} did not connect`));
